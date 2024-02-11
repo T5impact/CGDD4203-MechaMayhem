b8348c29bd4b10fde5feafd7a639a07fcb2524ad
Esutton24 <ethansutton24@gmail.com>
2024-02-11 17:35:52 -0500

    Main Menu Added
    Menu and settings screens added

---
+++ Mecha Mayhem/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Awake()
+    {
+        GetResolutions();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        if (resolutions == null)
+        {
+            GetResolutions();
+            if (resolutions == null) return;
+        }
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    void GetResolutions()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionsIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height)
+            {
+                currentResolutionsIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionsIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        print("Fullscreen: " + isFullscreen);
+        Screen.fullScreen = isFullscreen;
+    }
+}
