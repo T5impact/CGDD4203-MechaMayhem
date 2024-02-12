a81f5cb40204b321f40b04118d04e69e44a7c9fd
Luke Gamage <58281368+T5impact@users.noreply.github.com>
2024-02-12 23:02:35 +0900

    Fixed rotation problems, combined obstacle spawner with ground tile control

--- Mecha Mayhem/Assets/Scripts/GroundTileControl.cs
+++ Mecha Mayhem/Assets/Scripts/GroundTileControl.cs
@@ -7,11 +7,17 @@ public class TerrainGen : MonoBehaviour
     [Header("Generation Settings")]
     [SerializeField]
     List<Transform> obstacleSpawns;
+    // Assign these in the Inspector with the prefabs you want to spawn
+    [SerializeField] GameObject[] prefabsToSpawn;
+    //Probability of the tile spawning empty (no obstacles)
+    [SerializeField] [Range(0, 1)] float emptyChance = 0.5f;
 
     [Header("Movement Settings")]
     [SerializeField]
     float moveSpeed;
     public Transform origin; //Set in the script that spawns the ground tiles
+    [SerializeField] Vector3 endPosition = new Vector3(0, 0, -10f);
+
     Vector3 position;
 
 
@@ -19,6 +25,9 @@ public class TerrainGen : MonoBehaviour
     void Start()
     {
         position = this.transform.position;
+
+        if(Random.Range(0f, 1f) > emptyChance)
+            SpawnObstacle();
     }
 
     // Update is called once per frame
@@ -27,9 +36,36 @@ public class TerrainGen : MonoBehaviour
         this.transform.position = new Vector3(position.x, position.y, position.z - (moveSpeed * Time.deltaTime));
         position = this.transform.position;
 
-        if(position.z < -2.5f)
+        if(position.z < endPosition.z)
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        if (transform.childCount >= 3 && prefabsToSpawn.Length == 3)
+        {
+            // Select a random prefab index
+            int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+            // Get the transform of a randomly selected obstacle spawn
+            Transform selectedChild = obstacleSpawns[Random.Range(0, obstacleSpawns.Count)];
+
+            // Check if the prefab at the randomly selected index is not null
+            if (prefabsToSpawn[prefabIndex] != null)
+            {
+                // Instantiate the randomly selected prefab at the position and rotation of the selected child
+                Instantiate(prefabsToSpawn[prefabIndex], selectedChild.position, prefabsToSpawn[prefabIndex].transform.rotation, selectedChild);
+            }
+            else
+            {
+                Debug.LogError("The selected prefab is null. Please assign a prefab in the array.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Parent object needs at least three children and three prefabs assigned.");
         }
     }
 }
